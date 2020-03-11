ed3aa2233ee319b04aebfec575d6a62cd26febc6
Your Name <you@example.com>
2020-03-11 20:46:18 -0300

    Versao 3.0

--- Aluno.java
+++ Aluno.java
@@ -7,6 +7,12 @@ public String getNome() {
 public int getId() {
 	return id;
 }
+public void setNome(String nome) {
+	this.nome = nome;
+}
+public void setId(int id) {
+	this.id = id;
+}
    
 }
    
